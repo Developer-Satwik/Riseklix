870bc7436b731fd94d15a03400fbfbfd52159881
Satwik Kumar <59086799+Developer-Satwik@users.noreply.github.com>
2026-09-18 09:04:21 +0530

    chore: remove unused Buyer Situations link import

--- commercial-discovery-v1/app/(app)/projects/[id]/buyer-situations/page.tsx
+++ commercial-discovery-v1/app/(app)/projects/[id]/buyer-situations/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { createClient } from '@/lib/supabase/server'
 import { addIntentCandidate, approveIntent, rejectIntent } from './actions'
 import { generateBuyerIntents } from './generate-actions'
